f0beef20dcb77f411a000665f75022f264c1bebb
DevCarloss <=>
2023-05-16 13:07:20 -0300

    Adicionando novos arquivos e Alterando nomes

---
+++ nodejs/ordenar_lista_numeros.js
@@ -0,0 +1,12 @@
+// Logica de Programacao - Exercício 2 
+
+// Ordenar uma lista de números
+
+var lista = [5, 3, 2, 4, 7, 1, 0, 6]
+
+function ordenar_lista(){
+    var lista_ordenada = lista.sort()
+    return console.log(lista_ordenada)
+}
+
+ordenar_lista()
---
+++ python/ordenar_lista_numeros.py
@@ -0,0 +1,11 @@
+# Logica de Programacao - Exercicio 2
+
+# Ordenar lista de numeros
+
+lista_numeros = [5,1,4,3,6,7]
+
+def ordenar_lista_numeros():
+    lista_numeros.sort()
+    return print(lista_numeros)
+
+ordenar_lista_numeros()
